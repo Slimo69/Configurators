7ab662e159456a91e6e33840fb095ae0ceec7d45
Slimo69 <kosto.to@gmail.com>
2017-09-26 13:43:22 +0200

    Project Screen missing only deactivation of slider

--- main.py
+++ main.py
@@ -2,14 +2,27 @@ from classes import Project
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
+
+countries = ("Czechia", "Slovakia")
 
 class MainScreen(Screen):
     pass
 
 class ProjectScreen(Screen):
+    alt_checkbox = ObjectProperty()
+    alt_slider = ObjectProperty()
+    country_spinner = ObjectProperty()
+
+    def load_countries(self):
+        self.country_spinner.values = countries
+
     def printtxt(self, text):
         projects = Project(text)
         print projects.name
+        if self.alt_checkbox.active is True:
+            self.alt_checkbox.active = False
+            print self.alt_slider.value
 
 class ScreenManagement(ScreenManager):
     pass
@@ -18,6 +31,7 @@ presentation = Builder.load_file("main.kv")
 
 class MainApp(App):
     def build(self):
+        # country_spinner.values("Czechia", "Slovakia")
         return presentation
 
 ConfiguratorApp = MainApp()
